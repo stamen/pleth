6b69c63faa691803f3f6cc7de70b58a98618946e
Curran <curran.kelleher@gmail.com>
2020-06-15 14:10:57 -0400

    First pass polylabel

--- packages/examples/src/pleth/PolygonLabelsLayer.js
+++ packages/examples/src/pleth/PolygonLabelsLayer.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import polylabel from 'polylabel';
-//import { max } from 'd3-array';
+import React, { Fragment } from 'react';
+import polylabel from '@datavis-tech/polylabel';
+import { maxIndex } from 'd3-array';
 
 export const PolygonLabelsLayer = ({
   width,
@@ -23,6 +23,23 @@ export const PolygonLabelsLayer = ({
         coordinates: polylabel(feature.geometry.coordinates),
       });
     } else if (feature.geometry.type === 'MultiPolygon') {
+      const polylabels = feature.geometry.coordinates.map((polygon) =>
+        polylabel(polygon)
+      );
+      const coordinates = polylabels[maxIndex(polylabels, (d) => d.distance)];
+
+      if (coordinates === null) {
+        console.log(feature.geometry.coordinates);
+        return;
+      }
+
+      labels.push({
+        id: feature.id,
+        name: feature.properties.name,
+        coordinates,
+      });
+      //      console.log(polylabels);
+      //coordinates: polylabel(feature.geometry.coordinates),
       // TODO make it work for MultiPolygons.
       // Related: https://github.com/mapbox/polylabel/pull/61
       //console.log(feature.geometry.coordinates.map(coordinatespath.area));
@@ -37,23 +54,32 @@ export const PolygonLabelsLayer = ({
   return (
     <svg width={width} height={height}>
       {labels.map(({ id, name, coordinates }) => {
+        console.log(coordinates);
+        const projectedCoordinates = projection(coordinates);
+        if (projectedCoordinates === null) {
+          return null;
+        }
+        console.log(projectedCoordinates);
+        console.log(name);
         const [x, y] = projection(coordinates);
         return (
-          <text
-            key={id}
-            x={x}
-            y={y}
-            textAnchor="middle"
-            alignmentBaseline="middle"
-            // TODO use styled components
-            style={{
-              pointerEvents: 'none',
-              userSelect: 'none',
-              fontSize: '10px',
-            }}
-          >
-            {name}
-          </text>
+          <Fragment key={id}>
+            <circle cx={x} cy={y} r={2} />
+            <text
+              x={x}
+              y={y}
+              textAnchor="middle"
+              alignmentBaseline="middle"
+              // TODO use styled components
+              style={{
+                pointerEvents: 'none',
+                userSelect: 'none',
+                fontSize: '10px',
+              }}
+            >
+              {name}
+            </text>
+          </Fragment>
         );
       })}
     </svg>
