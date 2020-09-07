70e1c5b1d08b78779d95069dc36d89bc846fd930
Curran Kelleher <curran.kelleher@gmail.com>
2020-09-07 12:32:35 -0400

    Fix county-level scrape

--- packages/usCensusData/scrape.js
+++ packages/usCensusData/scrape.js
@@ -4,24 +4,39 @@ import { csvFormat } from 'd3-dsv';
 
 // See https://www.census.gov/data/developers/data-sets/popest-popproj/popest.html
 
-const getData = async ({ url, getFips }) => {
+const getData = async ({ url, parseRow }) => {
   const response = await fetch(url);
   const dataRaw = await response.json();
   const header = dataRaw.slice(0, 1)[0];
   const rows = dataRaw.slice(1);
-  return rows.map((row) => {
-    const d = header.reduce(
-      (accumulator, column, i) => ({ ...accumulator, [column]: row[i] }),
-      {}
-    );
-    return {
-      date: d.DATE_DESC.split(' ')[0],
-      fips: getFips(d),
-      population: +d.POP,
-      density: +d.DENSITY,
-    };
-  });
+  return rows.map((row) =>
+    parseRow(
+      header.reduce(
+        (accumulator, column, i) => ({ ...accumulator, [column]: row[i] }),
+        {}
+      )
+    )
+  );
 };
+
+const populationRow = (getFips) => (d) => {
+  const row = {
+    date: d.DATE_DESC.split(' ')[0],
+    population: +d.POP,
+    density: +d.DENSITY,
+  };
+  if (getFips) {
+    row.fips = getFips(d);
+  }
+  return row;
+};
+
+//const fipsToNameRow = (d) => ({
+//  date: d.DATE_DESC.split(' ')[0],
+//  fips: getFips(d),
+//  population: +d.POP,
+//  density: +d.DENSITY,
+//});
 
 const writeData = ({ fileName, data }) => {
   writeFileSync(fileName, csvFormat(data));
@@ -32,7 +47,7 @@ const urlBase = 'https://api.census.gov/data/2019/pep/population';
 const scrapeUS = async () => {
   const data = await getData({
     url: `${urlBase}?get=DATE_CODE,DATE_DESC,DENSITY,POP,NAME&for=us:*`,
-    getFips: (d) => d.us,
+    parseRow: populationRow(),
   });
   writeData({ fileName: 'data/us.csv', data });
 };
@@ -40,7 +55,7 @@ const scrapeUS = async () => {
 const scrapeStates = async () => {
   const data = await getData({
     url: `${urlBase}?get=COUNTY,DATE_CODE,DATE_DESC,DENSITY,POP,NAME,STATE&for=state:*`,
-    getFips: (d) => d.state,
+    parseRow: populationRow((d) => d.state),
   });
   writeData({ fileName: 'data/states.csv', data });
   return Array.from(new Set(data.map((d) => d.fips))).sort();
@@ -49,15 +64,25 @@ const scrapeStates = async () => {
 const scrapeCounties = async (stateFips) => {
   const data = await getData({
     url: `${urlBase}?get=COUNTY,DATE_CODE,DATE_DESC,DENSITY,POP&in=state:${stateFips}&for=county:*`,
-    getFips: (d) => d.state,
+    parseRow: populationRow((d) => stateFips + d.county),
   });
   writeData({ fileName: `data/counties_${stateFips}.csv`, data });
 };
+
+//const scrapeNames = async () => {
+//  const data = await getData({
+//    url: `${urlBase}?get=COUNTY,DATE_CODE,DATE_DESC,DENSITY,POP,NAME,STATE&for=state:*`,
+//    getFips: (d) => d.state,
+//  });
+//  writeData({ fileName: 'data/fipsToName.csv', data });
+//};
 
 const scrape = async () => {
   await scrapeUS();
   const states = await scrapeStates();
   states.forEach(scrapeCounties);
+
+  //scrapeNames();
 };
 
 scrape();
