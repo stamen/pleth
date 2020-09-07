1ebee348c7d79de57c7733cf01e0d2d93ece1959
Curran Kelleher <curran.kelleher@gmail.com>
2020-09-07 11:05:36 -0400

    Add states scraping script

--- packages/usCensusData/scrape.js
+++ packages/usCensusData/scrape.js
@@ -1,45 +1,53 @@
-import {writeFileSync} from 'fs';
+import { writeFileSync } from 'fs';
 import fetch from 'node-fetch';
 import { csvFormat } from 'd3-dsv';
 
-// See https://www.census.gov/data/developers/data-sets/popest-popproj/popest.html
-const scrape = async () => {
-  try {
-    const response = await fetch(
-      'https://api.census.gov/data/2019/pep/population?get=COUNTY,DATE_CODE,DATE_DESC,DENSITY,POP,NAME,STATE&for=us:*'
+const getData = async ({ url, getFips }) => {
+  const response = await fetch(url);
+  const dataRaw = await response.json();
+  const header = dataRaw.slice(0, 1)[0];
+  const rows = dataRaw.slice(1);
+  return rows.map((row) => {
+    const d = header.reduce(
+      (accumulator, column, i) => ({ ...accumulator, [column]: row[i] }),
+      {}
     );
-    const dataRaw = await response.json();
-    const header = dataRaw.slice(0, 1)[0];
-    const rows = dataRaw.slice(1);
-    const data = rows.map((row) => {
-      const d = header.reduce(
-        (accumulator, column, i) => ({ ...accumulator, [column]: row[i] }),
-        {}
-      );
-      // d = for example:
-      //  {
-      //    COUNTY: null,
-      //    DATE_CODE: '1',
-      //    DATE_DESC: '4/1/2010 Census population',
-      //    DENSITY: '87.38797508100000',
-      //    POP: '308745538',
-      //    NAME: 'United States',
-      //    STATE: null,
-      //    us: '1'
-      //  },
-      return {
-        date: d.DATE_DESC.split(' ')[0],
-        fips: d.us,
-        population: +d.POP,
-        density: +d.DENSITY,
-      };
-    });
+    return {
+      date: d.DATE_DESC.split(' ')[0],
+      fips: getFips(d),
+      population: +d.POP,
+      density: +d.DENSITY,
+    };
+  });
+};
 
-    const csv = csvFormat(data);
-    writeFileSync('1.csv', csv);
-  } catch (error) {
-    console.log(error);
-  }
+// See https://www.census.gov/data/developers/data-sets/popest-popproj/popest.html
+const scrapeUS = async () => {
+  const data = await getData({
+    url:
+      'https://api.census.gov/data/2019/pep/population?get=COUNTY,DATE_CODE,DATE_DESC,DENSITY,POP,NAME,STATE&for=us:*',
+    getFips: (d) => d.us,
+  });
+  const csv = csvFormat(data);
+  writeFileSync('data/us.csv', csv);
+};
+
+const scrapeStates = async () => {
+  const data = await getData({
+    url:
+      'https://api.census.gov/data/2019/pep/population?get=COUNTY,DATE_CODE,DATE_DESC,DENSITY,POP,NAME,STATE&for=state:*',
+    getFips: (d) => d.state,
+  });
+
+  const csv = csvFormat(data);
+  writeFileSync('data/states.csv', csv);
+  return Array.from(new Set(data.map((d) => d.fips))).sort();
+};
+
+const scrape = async () => {
+  await scrapeUS();
+  const states = await scrapeStates();
+  console.log(states);
 };
 
 scrape();
