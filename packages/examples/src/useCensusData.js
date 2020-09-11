14a228fef4a528f2bf2a342b11ae1fc58f975752
Curran Kelleher <curran.kelleher@gmail.com>
2020-09-11 11:57:59 -0400

    Fetch county data

--- packages/examples/src/useCensusData.js
+++ packages/examples/src/useCensusData.js
@@ -4,17 +4,33 @@ import { csv } from 'd3-fetch';
 import { timeParse } from 'd3-time-format';
 export const parseDate = timeParse('%m/%d/%Y');
 
-export const useCensusData = (activeId) => {
-  const { get } = useCache();
-  const censusData = get({
-    cacheKey: 'censusData' + activeId,
-    onCacheMiss: () =>
+const fips = (plethId) => plethId.substr(5);
+
+const fetchData = (activeId) => {
+  if (activeId === 'USA') {
+    return () =>
       csv('usCensusData/data/states.csv', (d) => ({
         date: parseDate(d.date),
         population: +d.population,
         density: +d.density,
         id: plethIdFIPS(d.fips),
-      })),
+      }));
+  } else {
+    return () =>
+      csv(`usCensusData/data/counties_${fips(activeId)}.csv`, (d) => ({
+        date: parseDate(d.date),
+        population: +d.population,
+        density: +d.density,
+        id: plethIdFIPS(d.fips),
+      }));
+  }
+};
+
+export const useCensusData = (activeId) => {
+  const { get } = useCache();
+  const censusData = get({
+    cacheKey: 'censusData' + activeId,
+    onCacheMiss: fetchData(activeId),
   });
   return censusData;
 };
