d4ab59e56b9503d7469f980acfaa2f4ac5f064b1
Curran Kelleher <curran.kelleher@gmail.com>
2020-09-11 13:44:03 -0400

    Finish URL state module.

--- packages/examples/src/pleth/useURLState.js
+++ packages/examples/src/pleth/useURLState.js
@@ -1,37 +1,29 @@
-import { useReducer } from 'react';
+import { useCallback } from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
 import queryString from 'query-string';
 
 export const useURLState = (urlStateConfig, reducer) => {
-  //const { search } = useLocation();
-  //const history = useHistory();
+  const { search } = useLocation();
+  const { push } = useHistory();
 
-  //const query = queryString.parse(search);
+  const query = queryString.parse(search);
 
-  //const urlState = Object.keys(query).reduce((accumulator, key) => {
-  //  const { defaultValue, parse } = urlStateConfig[key];
-  //  const value = query[key];
-  //  return {
-  //    ...accumulator,
-  //    [key]: value === undefined ? defaultValue : parse(value),
-  //  };
-  //}, {});
-
-  //const urlDispatch = history.push();
-
-  //return [urlState, urlDispatch];
-
-  // Initial state is default values.
-  const initialState = Object.keys(urlStateConfig).reduce(
-    (accumulator, key) => ({
+  const urlState = Object.keys(urlStateConfig).reduce((accumulator, key) => {
+    const { defaultValue, parse } = urlStateConfig[key];
+    const value = query[key];
+    return {
       ...accumulator,
-      [key]: urlStateConfig[key].defaultValue,
-    }),
-    {}
+      [key]: value === undefined ? defaultValue : parse(value),
+    };
+  }, {});
+
+  const urlDispatch = useCallback(
+    (action) =>
+      push({
+        search: queryString.stringify(reducer(urlState, action)),
+      }),
+    [push, urlState, reducer]
   );
 
-  // TODO build this out on react-router-dom and query-string.
-  // TODO make this a Context.
-  // Handle the case of a single action that sets multiple params (trickiest case, comes up in practice)
-  return useReducer(reducer, initialState);
+  return [urlState, urlDispatch];
 };
